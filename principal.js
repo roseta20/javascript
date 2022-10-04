fc6d3285cc3c922b9b2d0abe4759a88ecb70f3a4
roseta20 <105297781+roseta20@users.noreply.github.com>
2022-10-04 11:12:58 -0300

    Update principal.js

--- principal.js
+++ principal.js
@@ -1,20 +1,32 @@
-introducao-javascript
-├── css
-│   ├── index.css
-│   └── reset.css
-├── js
-│   └── principal.js
-├── img
-├── favicon.ico
-└── index.html
-// Restante do código HTML
-            </section>
-        </main>
-        <!-- Importação dos Javascripts AQUI -->
-        <script src="js/principal.js"></script>
-    </body>
-</html>
-<h1 class="titulo">Aparecida Nutrição</h1>
-var titulo = document.querySelector(".titulo");
-titulo.textContent = "Aparecida Nutricionista"
-var paciente = document.querySelector("#primeiro-paciente");
+var pacientes = document.querySelectorAll(".paciente");
+
+for(var i = 0; i < pacientes.length ; i++){
+    var paciente = pacientes[i];
+    var tdPeso = paciente.querySelector(".info-peso");
+    var tdAltura = paciente.querySelector(".info-altura");
+
+    var tdImc = paciente.querySelector(".info-imc");
+
+    var peso = tdPeso.textContent;
+    var altura = tdAltura.textContent;
+
+    var alturaEhValida = true;
+    var pesoEhValido = true;
+
+    if(peso <= 0 || peso >= 1000){
+        console.log("Peso inválido");
+        tdImc.textContent = "Peso inválido!";
+        pesoEhValido = false;
+    }
+
+    if(altura <= 0 || altura >= 3.0){
+        console.log("Altura inválida");
+        tdImc.textContent = "Altura inválida!";
+        alturaEhValida = false;
+    }
+
+    if(pesoEhValido && alturaEhValida){
+        var imc = peso / ( altura * altura);    
+        tdImc.textContent = imc;
+    }    
+}
