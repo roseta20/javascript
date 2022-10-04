2a0af84d074952aa16f34f4cf4a4a9a7861803d6
roseta20 <105297781+roseta20@users.noreply.github.com>
2022-10-04 10:59:22 -0300

    Create principal.js

---
+++ principal.js
@@ -0,0 +1,19 @@
+introducao-javascript
+├── css
+│   ├── index.css
+│   └── reset.css
+├── js
+│   └── principal.js
+├── img
+├── favicon.ico
+└── index.html
+// Restante do código HTML
+            </section>
+        </main>
+        <!-- Importação dos Javascripts AQUI -->
+        <script src="js/principal.js"></script>
+    </body>
+</html>
+<h1 class="titulo">Aparecida Nutrição</h1>
+var titulo = document.querySelector(".titulo");
+titulo.textContent = "Aparecida Nutricionista"
